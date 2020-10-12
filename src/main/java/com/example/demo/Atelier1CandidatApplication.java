3e40ad5967d237d374c36560a62a98bd59b5e608
Iheb <ihebmnasri@gmail.com>
2020-10-12 23:14:43 +0100

    First Commit.

---
+++ src/main/java/com/example/demo/Atelier1CandidatApplication.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import java.util.stream.Stream;
+
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class Atelier1CandidatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Atelier1CandidatApplication.class, args);
+	}
+
+	@Bean
+    ApplicationRunner init(CandidatRepository repository) {
+        return args -> {
+            Stream.of("Mariem", "Sarra", "Mohamed").forEach(nom -> {
+                repository.save(new Candidat(nom));
+            });
+            repository.findAll().forEach(System.out::println);
+        };
+    }
+
+	
+}
---
+++ src/test/java/com/example/demo/Atelier1CandidatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Atelier1CandidatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
